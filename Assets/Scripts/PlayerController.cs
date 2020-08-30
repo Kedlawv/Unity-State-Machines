5422dadd51aec9974368a5ff341e438557c59499
Kedlawv <waldek.bus@gmail.com>
2020-08-30 12:58:24 +0100

    Implementation of a naive approach to managing state

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,12 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer face;
     private Rigidbody rbody;
 
+    private bool isJumping;
+    private bool isDucking;
+    private bool isSpinning;
+
+    private float rotation;
+
     #endregion
 
     private void Awake()
@@ -31,11 +37,84 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping && !isDucking)
+            {
+                isJumping = true;
+                SetExpression(jumpingSprite);
+                rbody.AddForce(Vector3.up * jumpForce);
+            }
+            
+        }
+        else if (Input.GetButtonDown("Duck"))
+        {
+            if (!isJumping)
+            {
+                isDucking = true;
+                head.localPosition = new Vector3(head.localPosition.x, 0.5f, head.localPosition.z);
+                SetExpression(duckingSprite);
+            }
+            else
+            {
+                SetExpression(duckingSprite);
+                isSpinning = true;
+                SetExpression(jumpingSprite);
+            }
+        }
+        else if (Input.GetButtonUp("Duck"))
+        {
+            if (!isJumping)
+            {
+                isDucking = false;
+                head.localPosition = new Vector3(head.localPosition.x, 0.8f, head.localPosition.z);
+                SetExpression(idleSprite);
+            }
+        }
+        else if (Input.GetButtonDown("SwapWeapon"))
+        {
+            if(!isJumping && !isDucking && !isSpinning)
+            {
+                bool usingWeapon01 = weapon01.gameObject.activeInHierarchy;
+
+                weapon01.gameObject.SetActive(usingWeapon01 == false);
+                weapon02.gameObject.SetActive(usingWeapon01);
+            }
+        }
+
+        if (isSpinning)
+        {
+            Spin();
+        }
+    }
+
+    private void Spin()
+    {
+        float amountToRotate = 900 * Time.deltaTime;
+        rotation += amountToRotate;
+
+        if(rotation < 360)
+        {
+            transform.Rotate(Vector3.up, amountToRotate);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+            isSpinning = false;
+            rotation = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isJumping = false;
+        SetExpression(idleSprite);
     }
 
     public void SetExpression(Sprite newExpression)
     {
         face.sprite = newExpression;
     }
+
+    
 }
