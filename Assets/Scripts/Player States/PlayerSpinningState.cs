f29b367f3518ef93835a955dc3817eeb3b3479b2
Kedlawv <waldek.bus@gmail.com>
2020-09-03 09:20:47 +0200

    Context for FSM set up and 4 concrete state classes set up with a stub methods. Initial Idle state set up in the Start method. Finite State Machine Context -> Abstract State -> Concrete State | Context contains a reference to a variable of AbstractState type we assign an object of ConcreteState that implements the AbstractState. Context holds only one reference to a State class. readonly is Java's final. Member variables are pascal case in C#.

---
+++ Assets/Scripts/Player States/PlayerSpinningState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpinningState : PlayerBaseState
+{
+    public override void EnterState(PlayerController_FSM player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter(PlayerController_FSM player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(PlayerController_FSM player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
